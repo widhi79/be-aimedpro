cfe10c40e4f71afcae878fd5872525fb7c83439e
widhi79 <128588859+widhi79@users.noreply.github.com>
2024-03-17 14:33:35 +0700

    update setting.py allowed hosts

--- aimed/settings.py
+++ aimed/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-kc9olwt&*+nc&6$xwejj(x=420d$tebdea=@ol-)!@e11sqehk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", 'widhi79.github.io']
 
 # Application definition
 
